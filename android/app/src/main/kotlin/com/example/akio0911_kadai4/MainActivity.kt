dfdba564f71249b64dea94bd14d29d36134e24db
akio0911 <akio0911@gmail.com>
2021-04-18 10:37:30 +0900

    first commit

---
+++ android/app/src/main/kotlin/com/example/akio0911_kadai4/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.akio0911_kadai4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            _buildCounterLabel(),
+            _buildPlusButton(),
+            _buildClearButton(),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget _buildCounterLabel() {
+    return Text('$_counter');
+  }
+
+  Widget _buildPlusButton() {
+    return RaisedButton(
+      child: Text('+1'),
+      color: Colors.blue,
+      textColor: Colors.white,
+      onPressed: () {
+        setState(() {
+          _counter++;
+        });
+      },
+    );
+  }
+
+  Widget _buildClearButton() {
+    return RaisedButton(
+      child: Text('Clear'),
+      color: Colors.blue,
+      textColor: Colors.white,
+      onPressed: () {
+        setState(() {
+          _counter = 0;
+        });
+      },
+    );
+  }
+}
